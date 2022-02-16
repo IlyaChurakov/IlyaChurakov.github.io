bb38b2c7cc7372402809c05be52ff94e0f78dd19
Ilya Churakov <77941403+IlyaChurakov@users.noreply.github.com>
2022-02-17 00:37:18 +0300

    Add files via upload

--- Irvas/script.js
+++ Irvas/script.js
@@ -14044,13 +14044,26 @@ window.addEventListener('DOMContentLoaded', () => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 function forms() {
-  document.querySelectorAll('.regNum').forEach(item => {
-    regular(item); //Передаем инпут, который проверяем, и выражение-шаблон
+  const phonebtns = document.querySelectorAll('.regNum');
+  phonebtns.forEach(item => {
+    Regular(item, /\D/); //Передаем инпут, который проверяем, и выражение-шаблон
   });
 
-  function regular(input) {
+  function Regular(input, reg) {
     input.addEventListener('input', e => {
-      input.value = input.value.replace(/\D/, '');
+      let regex = reg;
+
+      if (input.value.match(regex)) {
+        //Проверяем на совпадение
+        let circle = input.value.length; //Берем изначальное значение длины
+
+        for (let i = 0; i < circle; i++) {
+          //Запускаем цикл, который пройдется столько раз, сколько было символов в самом начале
+          if (input.value.match(regex)) {
+            input.value = input.value.substring(0, input.value.length - 1); //Уберутся все символы после запретного до разрешенного то есть в выражении 123абв456 останется только 123, а в выражении абв123 не останется ничего
+          }
+        }
+      }
     });
   } //Отправка данных с формы
 
@@ -14482,6 +14495,7 @@ function timer(deadline) {
           hours = Math.floor((diff - days * 1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
           minutes = Math.floor((diff - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60) / (1000 * 60)),
           seconds = Math.floor((diff - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000);
+    alert(`${typeof diff}, ${typeof days}`);
     return {
       'total': diff,
       'days': days,
